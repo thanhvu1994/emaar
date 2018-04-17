b4c48970db082815aa3b39079a5c3204a079879f
Thanh Vu <vutt94@gmail.com>
2018-04-17 14:38:06 +0700

    delete all

--- wp-content/plugins/simple-post-se/inc/langs/zh.js
+++
@@ -1,3 +0,0 @@
-tinyMCE.addI18n("zh.series",{
-	title : '插入系列文章'
-});
--- wp-content/themes/build-wc/woocommerce.php
+++
@@ -1,7 +0,0 @@
-<?php get_header(); ?>
-<div class="container">
-    	<div class="pagelayout_area">
-			<?php woocommerce_content(); ?>
-		</div><!-- pagelayout_area -->
-</div><!-- container -->     
-<?php get_footer(); ?>
